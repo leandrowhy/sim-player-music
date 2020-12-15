734deda9bab1302209d389a05f9d492ee324f62b
WweiweiSmile <1119062049@qq.com>
2020-12-15 17:53:21 +0800

    云村初步搭建

---
+++ components/topBar.vue
@@ -0,0 +1,47 @@
+<template>
+	<view class="main">
+		<text class="menu">菜单</text>
+		<text class="content">
+			<text>关注</text>
+			<text>推荐</text>
+		</text>
+		<text class="release">添加</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	
+		
+	}
+</script>
+
+<style lang="scss">
+	.main{
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-between;
+		.menu{
+			background-color: #ff0000;
+		}
+		.content{
+			display: flex;
+			justify-content: space-between;
+			background-color: #39B54A;
+			& text{
+				margin-left: 40rpx;
+				&:first-child{
+					margin-left: 0rpx;
+				}
+			}
+		}
+		.release{
+			background-color: #007AFF;
+		}
+	}
+</style>
--- pages/cloudVillage/index.vue
+++ pages/cloudVillage/index.vue
@@ -1,8 +1,43 @@
+	
 <template>
-	<div class="class">这是一个页面</div>
+	<view>
+		<top-bar>
+		</top-bar>
+		<scroll-view>
+			<view>
+				<view class="head-portrait">头像</view>
+				<view class="release-content">
+					<view>name</view>
+					<view>2020-10-10</view>
+					<view>
+						xxxxxxx内容主体
+					</view>
+					<view>xxx的圈子</view>
+					<view>
+						<text><icon></icon>10</text>
+						<text><icon></icon>10</text>
+						<text><icon></icon>10</text>
+						<text><icon></icon></text>
+					</view>
+				</view>
+			</view>
+		</scroll-view>
+		
+	</view>
 </template>
-
-<script src="./js.js">
+<script>
+import topBar from "../../components/topBar.vue";
+	export default {
+		data() {
+			return {
+				
+			};
+		},
+		components: {
+		  // mainnavbar
+		  topBar
+		},
+	}
 </script>
-<style lang="scss" scoped src="./css.scss">
+<style scoped>
 </style>
